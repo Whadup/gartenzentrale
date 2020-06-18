d40c69a4845902d035667711a9220cf86b02bbcd
Your Name <you@example.com>
2020-06-18 21:04:11 +0100

    terminate child processes

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGTERM
 while true
 do
-	echo "(Re-)Starting Garten Server"
-    python3 gartenzentrale.py
+    echo "(Re-)Starting Garten Server"
+    python3 gartenzentrale.py&
+    child=$!
+    wait "$child"
 	sleep 1
 done
